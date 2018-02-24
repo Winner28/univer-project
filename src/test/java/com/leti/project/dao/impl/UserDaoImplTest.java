941c32fe783d5f9675729f1148993183127dadbd
Vlad Bagdey <Vladislav_Bagdei@epam.com>
2018-02-24 16:27:27 +0300

    univer-project: finally added dependencies that solve my problem with f.. spring extensions class

---
+++ src/test/java/com/leti/project/dao/impl/UserDaoImplTest.java
@@ -0,0 +1,13 @@
+package com.leti.project.dao.impl;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+@ExtendWith(SpringExtension.class)
+class UserDaoImplTest {
+
+
+}
