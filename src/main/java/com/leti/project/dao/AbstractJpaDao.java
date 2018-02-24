801113ea4a687f60a9523a007352b98858911bdf
Vlad Bagdey <Vladislav_Bagdei@epam.com>
2018-02-24 15:07:27 +0300

    univer-project: added necessary methods to AJD

--- src/main/java/com/leti/project/dao/AbstractJpaDao.java
+++ src/main/java/com/leti/project/dao/AbstractJpaDao.java
@@ -6,7 +6,10 @@ import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import java.util.function.Consumer;
 import java.util.function.Function;
+import java.util.function.Supplier;
 
 @AllArgsConstructor(onConstructor = @__(@Autowired))
 public abstract class AbstractJpaDao {
@@ -17,5 +20,30 @@ public abstract class AbstractJpaDao {
         final EntityManager entityManager = emf.createEntityManager();
         return entityManagerMapper.apply(entityManager);
     }
+
+    protected <T> T mapEntityManagerTransactional(Function<EntityManager, T> entityManagerMapper) {
+        return mapEntityManager(entityManager -> {
+            EntityTransaction entityTransaction = entityManager.getTransaction();
+            entityTransaction.begin();
+            T result = entityManagerMapper.apply(entityManager);
+            entityTransaction.commit();
+            return result;
+        });
+    }
+
+    protected void withEntityManager(Consumer<EntityManager> entityManagerSupplier) {
+        final EntityManager entityManager = emf.createEntityManager();
+        entityManagerSupplier.accept(entityManager);
+    }
+
+    protected void withEntityManagerTransactional(Consumer<EntityManager> entityManagerTransactional) {
+        withEntityManager(entityManager -> {
+            EntityTransaction transaction = entityManager.getTransaction();
+            transaction.begin();
+            entityManagerTransactional.accept(entityManager);
+            transaction.commit();
+        });
+    }
+
 
 }
